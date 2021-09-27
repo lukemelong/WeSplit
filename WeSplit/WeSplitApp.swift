d17e0965bed4b2d8a8ced7e3658d1bf44479878e
Luke Melong <lukemelong@protonmail.com>
2021-09-27 16:28:24 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Luke Melong on 2021-09-27.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
